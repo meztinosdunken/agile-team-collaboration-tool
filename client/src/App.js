4a927fe5a0a9c1dbe1f30ec2d6cc9377003f327f
meztinosdunken <orlanmorry5462@hotmail.com>
2025-09-04 12:40:03 -0500

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Agile Team Collaboration Tool</h1>
+      <p>Empowering agile teams to collaborate effectively.</p>
+    </div>
+  );
+}
+
+export default App;
